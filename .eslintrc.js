2c43647343ccf3f269ba6a11d0a472fd2331ed69
liradb2000 <liradb2000@live.co.kr>
2026-02-27 23:41:33 +0900

    chore: update version to 4.0.0 and adjust node engine requirement fix: refine ESLint rules for import dependencies refactor: clean up husky hooks configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: ["@webpack-contrib/eslint-config-webpack", "prettier"],
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: false, // Disallow devDependencies in source code
+        optionalDependencies: false,
+        peerDependencies: false,
+        bundledDependencies: false,
+        includeInternal: false,
+        includeTypes: false,
+      },
+    ],
+  },
 };
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  globalSetup: "<rootDir>/globalSetupTest.js",
-  setupFilesAfterEnv: ["<rootDir>/setupTest.js"],
-};
